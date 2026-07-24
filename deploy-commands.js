945ea881660be18e2baa0217cb3008096f29e119
shakesadbee <shakesadbee@gmail.com>
2026-07-24 09:18:20 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,47 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
-
 const commands = [
   new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with Pong!")
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName("order")
+    .setDescription("Create a new order")
+    .addUserOption(option =>
+      option
+        .setName("customer")
+        .setDescription("Customer of the order")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("service")
+        .setDescription("Service name")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("price")
+        .setDescription("Order price")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("details")
+        .setDescription("Order details")
+        .setRequired(true)
+    )
+    .addChannelOption(option =>
+      option
+        .setName("channel")
+        .setDescription("Channel to send order")
+        .setRequired(true)
+    )
+    .addAttachmentOption(option =>
+      option
+        .setName("image")
+        .setDescription("Order image")
+        .setRequired(false)
+    )
     .toJSON()
 ];
-
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
-(async () => {
-  try {
-    console.log("Registering commands...");
-
-    await rest.put(
-      Routes.applicationGuildCommands(
-        "1530223484599533689",
-        "1522931483587776552"
-      ),
-      { body: commands }
-    );
-
-    console.log("Commands registered!");
-  } catch (error) {
-    console.error(error);
-  }
-})();

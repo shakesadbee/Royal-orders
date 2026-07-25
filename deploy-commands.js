7aa5dd54d5473ddc23fc6aaf402d93a7c211bcf9
shakesadbee <shakesadbee@gmail.com>
2026-07-25 05:20:09 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,53 +25,122 @@ const commands = [
     )
     .toJSON(),
 
+
   new SlashCommandBuilder()
     .setName("say")
     .setDescription("Make the bot say something")
     .addStringOption(option =>
-      option.setName("message").setDescription("Message to send").setRequired(true)
+      option
+        .setName("message")
+        .setDescription("Message to send")
+        .setRequired(true)
     )
     .toJSON(),
+
 
   new SlashCommandBuilder()
     .setName("vouch")
     .setDescription("Give a vouch to a user")
     .addUserOption(option =>
-      option.setName("user").setDescription("User to vouch").setRequired(true)
+      option
+        .setName("user")
+        .setDescription("User to vouch")
+        .setRequired(true)
     )
     .addStringOption(option =>
-      option.setName("message").setDescription("Vouch message").setRequired(true)
+      option
+        .setName("message")
+        .setDescription("Vouch message")
+        .setRequired(true)
     )
     .addChannelOption(option =>
-      option.setName("channel").setDescription("Channel to send the vouch").setRequired(true)
+      option
+        .setName("channel")
+        .setDescription("Channel to send the vouch")
+        .setRequired(true)
     )
     .toJSON(),
+
 
   new SlashCommandBuilder()
     .setName("vouchcheck")
     .setDescription("Check a user's vouches")
     .addUserOption(option =>
-      option.setName("user").setDescription("User to check").setRequired(true)
+      option
+        .setName("user")
+        .setDescription("User to check")
+        .setRequired(true)
+    )
+    .toJSON(),
+
+
+  new SlashCommandBuilder()
+    .setName("giveaway")
+    .setDescription("Create a giveaway")
+    .addStringOption(option =>
+      option
+        .setName("prize")
+        .setDescription("Giveaway prize")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("duration")
+        .setDescription("Duration example: 1m, 1h, 1d")
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("winners")
+        .setDescription("Number of winners")
+        .setRequired(true)
+    )
+    .addChannelOption(option =>
+      option
+        .setName("channel")
+        .setDescription("Giveaway channel")
+        .setRequired(true)
+    )
+    .toJSON(),
+
+
+  new SlashCommandBuilder()
+    .setName("reroll")
+    .setDescription("Reroll a giveaway")
+    .addStringOption(option =>
+      option
+        .setName("id")
+        .setDescription("Giveaway ID")
+        .setRequired(true)
     )
     .toJSON()
 
 ];
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+const rest = new REST({ version: "10" })
+  .setToken(process.env.TOKEN);
+
 
 (async () => {
   try {
+
     console.log("Registering GLOBAL commands...");
 
     await rest.put(
       Routes.applicationCommands(
         "1530223484599533689"
       ),
-      { body: commands }
+      {
+        body: commands
+      }
     );
 
     console.log("✅ Global commands registered!");
+
   } catch (error) {
+
     console.error(error);
+
   }
 })();

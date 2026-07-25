3dc975d8a47d8c3327567104fa582f06db25a02d
shakesadbee <shakesadbee@gmail.com>
2026-07-25 03:37:04 -0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,93 +10,55 @@ const commands = [
   new SlashCommandBuilder()
     .setName("order")
     .setDescription("Create a new order")
-
     .addUserOption(option =>
-      option
-        .setName("customer")
-        .setDescription("Select customer")
-        .setRequired(true)
+      option.setName("customer").setDescription("Select customer").setRequired(true)
     )
-
     .addStringOption(option =>
-      option
-        .setName("service")
-        .setDescription("Service name")
-        .setRequired(true)
+      option.setName("service").setDescription("Service name").setRequired(true)
     )
-
     .addStringOption(option =>
-      option
-        .setName("price")
-        .setDescription("Order price")
-        .setRequired(true)
+      option.setName("price").setDescription("Order price").setRequired(true)
     )
-
     .addStringOption(option =>
-      option
-        .setName("details")
-        .setDescription("Order details")
-        .setRequired(true)
+      option.setName("details").setDescription("Order details").setRequired(true)
     )
-
     .addChannelOption(option =>
+      option.setName("channel").setDescription("Channel where order will be sent").setRequired(true)
+    )
+    .addAttachmentOption(option =>
+      option.setName("image").setDescription("Add order image").setRequired(false)
+    )
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("Make the bot say something")
+    .addStringOption(option =>
       option
-        .setName("channel")
-        .setDescription("Channel where order will be sent")
+        .setName("message")
+        .setDescription("Message to send")
         .setRequired(true)
     )
-
-    .addAttachmentOption(option =>
-      option
-        .setName("image")
-        .setDescription("Add order image")
-        .setRequired(false)
-    )
-
     .toJSON()
+
 ];
 
-
-const rest = new REST({ version: "10" })
-  .setToken(process.env.TOKEN);
-
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-
   try {
-
     console.log("Registering commands...");
 
     await rest.put(
-
       Routes.applicationGuildCommands(
         "1530223484599533689",
         "1522931483587776552"
       ),
-
-      {
-        body: commands
-      }
-
+      { body: commands }
     );
 
     console.log("✅ Commands registered successfully!");
-
   } catch (error) {
-
-    console.error("❌ Error registering commands:");
     console.error(error);
-
   }
-
 })();
-new SlashCommandBuilder()
-  .setName("say")
-  .setDescription("Make the bot say something")
-  .addStringOption(option =>
-    option
-      .setName("message")
-      .setDescription("Message to send")
-      .setRequired(true)
-  )
-  .toJSON()
